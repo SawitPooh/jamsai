e5747e588a4d40014b7bd69cfa02a77401b396b4
SawitPooh <sawit.j@kkumail.com>
2025-05-25 23:07:19 +0700

    delete none of this web

--- app/form/end_jamsai/select_only_doctor/page.tsx
+++ app/form/end_jamsai/select_only_doctor/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 
 export default function SelectDoctorChatPage() {
-  const router = useRouter();
+
 
   const handleBookDoctor = () => {
     window.open(
